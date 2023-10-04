54bdad736e9de8f90ea788963d5a07f4169c959f
DovgalNikita <nikitadovgalkubsau@yandex.ru>
2023-10-05 00:03:32 +0300

    started galleryScreen

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './MainPage.module.scss';
 import WelcomeScreen from '../../screens/WelcomeScreen/WelcomeScreen';
 import FeaturesScreen from '../../screens/FeaturesScreen/FeaturesScreen';
+import GalleryScreen from '../../screens/GalleryScreen/GalleryScreen';
 
 const MainPage = () => {
     return(
         <main className={styles.main}>
             <WelcomeScreen />
             <FeaturesScreen />
+            <GalleryScreen />
         </main>
     )
 }
---
+++ src/screens/GalleryScreen/GalleryScreen.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import styles from './GalleryScreen.module.scss';
+
+
+const GalleryScreen = () => {
+    return(
+        <div className={styles.screen}>
+            <div className={styles.title}>Эксклюзивный доступ к событиям Самокатов</div>
+
+        </div>
+    )
+}
+
+export default GalleryScreen;
