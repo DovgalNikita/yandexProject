8af26b74fc7e4684339198d0326ee7a90d3ec151
DovgalNikita <nikitadovgalkubsau@yandex.ru>
2023-10-05 00:43:48 +0300

    Saved changes

--- src/screens/GalleryScreen/GalleryScreen.tsx
+++ src/screens/GalleryScreen/GalleryScreen.tsx
@@ -6,7 +6,11 @@ const GalleryScreen = () => {
     return(
         <div className={styles.screen}>
             <div className={styles.title}>Эксклюзивный доступ к событиям Самокатов</div>
-
+            <p className={styles.caption}>
+                С абонементом — у вас доступ <br />к специальным мероприятиям, <br />скидки и классный мерч. 
+                В 2023 году <br /> мы танцевали на Даче Плюс <br /> с Антохой MC, катались на самокатах <br />с 
+                Сергеем Мезенцевым и слушали хор <br />Attaque de Panique
+            </p>
         </div>
     )
 }
