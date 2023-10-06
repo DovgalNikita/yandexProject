4341c42e08b86b14caa0db20ba4f18a3baa14842
DovgalNikita <nikitadovgalkubsau@yandex.ru>
2023-10-06 22:21:55 +0300

    desktop 2.0

--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -11,7 +11,7 @@ const WelcomeScreen: FC = () => {
             <div className={styles.contentBlock}>
                 <Logo width={"176px"} height={"50.13px"}/>
                 <p className={styles.title}>Вступайте в клуб <br />Самокатов</p>
-                <p className={styles.caption}>Купите абонемент, чтобы стать частью клуба в новом сезоне-2024. <br /> 8 месяцев за <span className={styles.curPrice}>499₽<span className={styles.prevPrice}>3190₽</span></span></p>
+                <p className={styles.caption}>Купите абонемент, чтобы стать частью клуба в новом сезоне-2024. <br /> 8 месяцев за <span className={styles.price}><span className={styles.curPrice}>499₽</span><span className={styles.prevPrice}>3190₽</span></span></p>
                 <a href="https://go.yandex/ru_ru/lp/rides/scooter" target="_blank" rel="noreferrer"><StyledButton margin="44px 0 0">Стать частью клуба</StyledButton></a>
             </div>
             <div className={styles.illustrationBlock}>
